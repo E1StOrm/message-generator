75092e54ed67009be8ea8f706139b4d6dc6fd687
Denys Korobov <denys.korobov@hotmail.com>
2024-11-05 00:06:06 +0100

    Add message animation

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ window.addEventListener("load", (evt) => {
     const rndMessage = messages[Math.floor(Math.random() * messages.length)];
     
     const quote = document.createElement("blockquote");
-    quote.appendChild(document.createTextNode(rndMessage.message));
+    //quote.appendChild(document.createTextNode(rndMessage.message));
     
     const small = document.createElement("small");
     small.appendChild(document.createTextNode(rndMessage.author));
@@ -13,4 +13,12 @@ window.addEventListener("load", (evt) => {
     const container = document.getElementById("message");
     container.appendChild(quote);
     container.appendChild(small);
+
+    let index = 0;
+    let interval = setInterval(() => {
+        quote.appendChild(document.createTextNode(rndMessage.message.charAt(index++)));
+        if (index >= rndMessage.message.length) {
+            clearInterval(interval);
+        }
+    }, 30);
 });
